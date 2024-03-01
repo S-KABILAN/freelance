713eaa7f08a3b4bb4ad53b9e30bba1d4e7907728
kabilan-s <kabilanselvakumar313@gmail.com>
2024-03-01 20:15:16 +0530

    commit

--- pages/home/components/Ourofferings.jsx
+++ pages/home/components/Ourofferings.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import offeringsData from "./componentsdata/Ourofferingsdata";
+import offeringsData from "../componentsdata/Ourofferingsdata";
 
 const Ourofferings = () => {
   return (
